751b50e95fd0f3b7d10d05d4ee1aa70c380fef18
Nikita Chekalin <chekalin-kanztorg@yandex.ru>
2025-02-09 01:05:54 +0300

    Add Put and Delete Handlers

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -37,6 +38,48 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(task)
 }
 
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	var message Message
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
+
+	if err := DB.First(&message, message.ID).Error; err != nil {
+		http.Error(w, "Task not found", http.StatusNotFound)
+		return
+	}
+
+	DB.Delete(&message, message.ID)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var message Message
+
+	var updates map[string]interface{}
+
+	err := json.NewDecoder(r.Body).Decode(&updates)
+	if err != nil {
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
+
+	updates["UpdatedAt"] = time.Now()
+
+	if err := DB.First(&message, updates["ID"]).Error; err != nil {
+		http.Error(w, "Task not found", http.StatusNotFound)
+		return
+	}
+
+	DB.Model(&message).Where("id = ?", message.ID).Updates(updates)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
 func main() {
 
 	InitDB()
@@ -47,6 +90,8 @@ func main() {
 
 	router.HandleFunc("/api/messages", PostHandler).Methods("POST")
 	router.HandleFunc("/api/messages", GetHandler).Methods("GET")
+	router.HandleFunc("/api/messages", DeleteHandler).Methods("DELETE")
+	router.HandleFunc("/api/messages", UpdateTaskHandler).Methods("PUT")
 
 	http.ListenAndServe("localhost:8080", router)
 

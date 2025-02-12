f5a9e7c2f6414f3683ac94131250e920b74da38f
Nikita Chekalin <chekalin-kanztorg@yandex.ru>
2025-02-12 12:49:04 +0300

    Fix task 3

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -39,42 +40,55 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
 	var message Message
-	err := json.NewDecoder(r.Body).Decode(&message)
+
+	taskID, err := strconv.ParseUint(id, 10, 32)
 	if err != nil {
-		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
-	if err := DB.First(&message, message.ID).Error; err != nil {
+	if err := DB.First(&message, uint(taskID)).Error; err != nil {
 		http.Error(w, "Task not found", http.StatusNotFound)
 		return
 	}
 
-	DB.Delete(&message, message.ID)
+	DB.Delete(&message, uint(taskID))
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(message)
 }
 
 func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var message Message
+	vars := mux.Vars(r)
+	id := vars["id"]
 
+	taskID, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var message Message
 	var updates map[string]interface{}
 
-	err := json.NewDecoder(r.Body).Decode(&updates)
-	if err != nil {
+	errP := json.NewDecoder(r.Body).Decode(&updates)
+	if errP != nil {
 		http.Error(w, "Invalid Request", http.StatusBadRequest)
 		return
 	}
 
 	updates["UpdatedAt"] = time.Now()
 
-	if err := DB.First(&message, updates["ID"]).Error; err != nil {
+	if err := DB.First(&message, uint(taskID)).Error; err != nil {
 		http.Error(w, "Task not found", http.StatusNotFound)
 		return
 	}
 
-	DB.Model(&message).Where("id = ?", message.ID).Updates(updates)
+	DB.Model(&message).Where("ID = ?", uint(taskID)).Updates(updates)
+	DB.Save(&message)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(message)
@@ -90,8 +104,8 @@ func main() {
 
 	router.HandleFunc("/api/messages", PostHandler).Methods("POST")
 	router.HandleFunc("/api/messages", GetHandler).Methods("GET")
-	router.HandleFunc("/api/messages", DeleteHandler).Methods("DELETE")
-	router.HandleFunc("/api/messages", UpdateTaskHandler).Methods("PUT")
+	router.HandleFunc("/api/messages/{id}", DeleteHandler).Methods("DELETE")
+	router.HandleFunc("/api/messages/{id}", UpdateTaskHandler).Methods("PUT")
 
 	http.ListenAndServe("localhost:8080", router)
 
